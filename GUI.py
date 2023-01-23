fefe3f2799eebf3e257962aa0ace16aa9ffcb837
Nathan Savoy <nsavoy7@gmail.com>
2023-01-23 11:28:14 -0500

    GUI colour update

--- GUI.py
+++ GUI.py
@@ -2,15 +2,17 @@ from tkinter import *
 import tkinter.messagebox as mb
 from tkinter import filedialog as fd
 from PIL import ImageTk,Image
-import signal_processing.analysis as sp
+import src.analysis as sp
+from src.params import *
 
 
 ### INITIALIZATION ###
 ###
+
 root = Tk()
 root.geometry('800x450')
 root.title('Audio ToyBox')
-root.config(bg='gray21')
+root.config(bg=sp.frg_clr)
 filename = StringVar()
 filename.set("No File Selected")
 bpm = StringVar()
@@ -68,57 +70,57 @@ def do_zoom(event):
 ### GUI ELEMENTS ###
 ###
 ## FRAMES ##
-upper_frame = Frame(root, width=780, height=110, bg="gray64")
+upper_frame = Frame(root, width=780, height=110, bg=bkg_clr)
 upper_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
-lower_frame = Canvas(root, width=780, height=300, bg="gray64")
+lower_frame = Canvas(root, width=780, height=300, bg=bkg_clr)
 lower_frame.pack(fill="x", expand=False, padx=10, pady=10)
 
 # upper frame elements 
 ## sub-frames 
 # rows
-uf_row0 = Frame(upper_frame, width=660, height=20, bg="gray64")
+uf_row0 = Frame(upper_frame, width=660, height=20, bg=bkg_clr)
 uf_row0.pack(fill="x", expand=True, padx=4, pady=0)
-uf_row1 = Frame(upper_frame, width=660, height=20, bg="gray64")
+uf_row1 = Frame(upper_frame, width=660, height=20, bg=bkg_clr)
 uf_row1.pack(fill="x", expand=True, padx=4, pady=0)
-uf_row2 = Frame(upper_frame, width=660, height=40, bg="gray64")
+uf_row2 = Frame(upper_frame, width=660, height=40, bg=bkg_clr)
 uf_row2.pack(fill="x", expand=True, padx=4, pady=5)
 
-#lf_row0 = Canvas(lower_frame, width=700, height=90, bg="gray64")
+#lf_row0 = Canvas(lower_frame, width=700, height=90, bg=bkg_clr)
 #lf_row0.pack(fill="x", expand=True, padx=0, pady=5)
 #lf_row1 = Canvas(lower_frame, width=700, height=90, bg="red")
 #lf_row1.pack(fill="x", expand=True, padx=0, pady=5)
 
 # columns
-uf_row1_c0 = Frame(uf_row1, bg="gray64")
+uf_row1_c0 = Frame(uf_row1, bg=bkg_clr)
 uf_row1_c0.place(relx=.5, rely=.5,anchor= CENTER)
 
-uf_row2_c0 = Frame(uf_row2, bg="gray64")
+uf_row2_c0 = Frame(uf_row2, bg=bkg_clr)
 uf_row2_c0.place(relx=.5, rely=.5,anchor= CENTER)
 
 ## widgets 
-title = Label(uf_row0, bd=0, textvariable=filename, width=50, height=1, bg="gray64")
+title = Label(uf_row0, bd=0, textvariable=filename, width=50, height=1, bg=bkg_clr)
 title.pack(fill="both", expand=True)
 
-bpm_lab = Label(uf_row1_c0, bd=0, text="BPM:", bg="gray64")
+bpm_lab = Label(uf_row1_c0, bd=0, text="BPM:", bg=bkg_clr)
 bpm_lab.grid(row=0, column=0, padx=10, pady=0)
-bpm_val = Label(uf_row1_c0, bd=0, textvariable=bpm, width=5, height=1, bg="gray64")
+bpm_val = Label(uf_row1_c0, bd=0, textvariable=bpm, width=5, height=1, bg=bkg_clr)
 bpm_val.grid(row=0, column=1, padx=10, pady=0)
 
-key_lab = Label(uf_row1_c0, bd=0, text="Key:", bg="gray64")
+key_lab = Label(uf_row1_c0, bd=0, text="Key:", bg=bkg_clr)
 key_lab.grid(row=0, column=2, padx=10, pady=0)
-key_val = Label(uf_row1_c0, bd=0, textvariable=key, width=10, height=1, bg="gray64")
+key_val = Label(uf_row1_c0, bd=0, textvariable=key, width=10, height=1, bg=bkg_clr)
 key_val.grid(row=0, column=3, padx=10, pady=0)
 
-norm_lab = Label(uf_row1_c0, bd=0, text="Loudness:", bg="gray64")
+norm_lab = Label(uf_row1_c0, bd=0, text="Loudness:", bg=bkg_clr)
 norm_lab.grid(row=0, column=4, padx=10, pady=0)
-norm_val = Label(uf_row1_c0, bd=0, textvariable=norm, width=7, height=1, bg="gray64")
+norm_val = Label(uf_row1_c0, bd=0, textvariable=norm, width=7, height=1, bg=bkg_clr)
 norm_val.grid(row=0, column=5, padx=10, pady=0)
 
-import_btn = Button(uf_row2_c0, text="Import New", width=10, height=1, command=import_audio, bg="red3")
+import_btn = Button(uf_row2_c0, text="Import New", width=10, height=1, command=import_audio, bg=hlt3_clr)
 import_btn.grid(row=0, column=0, padx=10, pady=5)
 
-analyze_btn = Button(uf_row2_c0, text="Analyze Audio", width=15, height=1, command=analyze_audio, bg="green3")
+analyze_btn = Button(uf_row2_c0, text="Analyze Audio", width=15, height=1, command=analyze_audio, bg=hlt2_clr)
 analyze_btn.grid(row=0, column=1, padx=10, pady=5)
 
 amp_cont = lower_frame.create_image(-50, 0, anchor=NW)
@@ -129,29 +131,4 @@ lower_frame.bind("<MouseWheel>", do_zoom)
 lower_frame.bind('<ButtonPress-1>', lambda event: lower_frame.scan_mark(event.x, event.y))
 lower_frame.bind("<B1-Motion>", lambda event: lower_frame.scan_dragto(event.x, event.y, gain=1))
 
-
-
-'''
-# button
-import_btn = Button(
-    root, 
-    text="Import Audio", 
-    width=10, 
-    height=2, 
-    command=import_audio
-)
-import_btn.place(x=400, y=200)
-import_btn.pack(padx=30,pady=30)
-
-# label
-#lab = Label(root, bd=20, textvariable=filename, width=20, height=2)
-#lab.place(x=400, y=200)
-
-# image frame
-frame = Frame(root, width=700, height=150, background='white')
-frame.pack_propagate(0)    
-frame.pack(padx=30, pady=30)
-label = Label(frame, image='')
-label.pack()
-'''
 root.mainloop()
